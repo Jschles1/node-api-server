ea001a77ee249a12e001962d53a394c182068851
Jschles1 <jschles1@ramapo.edu>
2018-01-16 23:12:47 -0500

    Removed pw import

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const { pw } = require('../../info');
-
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 // process.env.MONGO_URI
